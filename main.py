afa1331f14e49cc13d1de27b822413c78c8c7b08
Daniel Aguilar-Rodriguez <daniel.a-r@outlook.com>
2022-12-15 20:20:33 -0500

    Combine dataframes and create functions

--- main.py
+++ main.py
@@ -2,6 +2,94 @@ import pandas as pd
 import numpy as np
 import os
 import requests
+import datetime
+import json
+import re
+import time 
+
+def get_json(url):
+    response = requests.get(url)
+    j = json.loads(response.text)
+    
+    return j
+
+def get_code(target_value, url):
+    code = ''
+    j = get_json(url)
+    
+    for entry in j['Data']:
+        if entry['value_represented'] == target_value:
+            code = entry['code']
+            
+    return code
+
+def get_state_code(state_name):
+    url = 'https://aqs.epa.gov/data/api/list/states?email=test@aqs.api&key=test'
+    state_code = get_code(state_name, url)
+
+    return state_code
+
+def get_county_code(state_code, county_name):
+    url = f'https://aqs.epa.gov/data/api/list/countiesByState?email=test@aqs.api&key=test&state={state_code}'
+    county_code = get_code(county_name, url)
+    
+    return county_code
+
+'''def get_county_code_list(county_names):
+    county_codes = []
+    for county in county_names:
+        county_codes.append()'''
+
+def get_county_code_list(state_code):
+    url = f'https://aqs.epa.gov/data/api/list/countiesByState?email=test@aqs.api&key=test&state={state_code}'
+
+    j = get_json(url)
+    county_codes = []
+    
+    for county in j['Data']:
+        if re.fullmatch('Bronx|Kings|New York|Queens|Richmond', county['value_represented']):
+            county_codes.append(county['code'])  
+    
+    return county_codes
+
+
+#start_date = str(traffic_vol_daily['date'].min()).replace('-', '')
+#end_date = str(traffic_vol_daily['date'].max()).replace('-', '')
+#start_date = '20160101'
+#end_date = '20160229'
+#county_code = '005'
+
+def get_daily_air_quality_list(state_code, county_codes):
+    email = 'daguila000@citymail.cuny.edu'
+    key = 'cobaltcrane81'
+    param_code = '88101'
+    daily_air_quality_list = []
+
+    #year_counter = traffic_vol_daily['date'].min().year
+    #end_year = traffic_vol_daily['date'].max().year
+    year_counter = 2009
+    end_year = 2020
+
+    while year_counter <= end_year:
+        for county_code in county_codes: 
+            start_date = str(year_counter) + '0101'
+            end_date = str(year_counter) + '1231'
+            url = f'https://aqs.epa.gov/data/api/dailyData/byCounty?email={email}&key={key}&param={param_code}&bdate={start_date}&edate={end_date}&state={state_code}&county={county_code}'
+            j = get_json(url)
+            print(j['Header'])
+            daily_air_quality_list.extend(j['Data'])
+            time.sleep(6)
+        year_counter += 1
+        
+    return daily_air_quality_list
+
+def create_air_quality_csv():
+    state_code = get_state_code(state)
+    county_code_list = get_county_code_list(state_code)
+    daily_air_quality_list = get_daily_air_quality_list(county_code_list)
+    daily_air_quality_df = pd.DataFrame(daily_air_quality_list)
+    daily_air_quality_df.to_csv('datasets/daily_air_quality.csv')
+
 
 def download_data(csv_name):
     """Download data from link matching key in dictionary and save in datasets folder
